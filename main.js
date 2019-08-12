6c5b4961f6e6d9b211d80e20afec5eb0e55a8be7
yuhan <yuhan.gao@timecitytech.com>
2019-08-12 15:38:11 +0800

    update

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const ul = document.querySelector('ul')
 const lightbox = document.querySelector('.lightbox')
 const lightboxImg = document.querySelector('.lightboxImg')
-const imgNUm = document.querySelector('.imgNUm')
+const imgNum = document.querySelector('.imgNum')
+const imgLen = document.querySelector('.imgLen')
 const foxImg = ['img/fox0.jpg', 'img/fox1.jpg', 'img/fox2.jpg', 'img/fox3.jpg', 'img/fox4.jpg', 'img/fox5.jpg']
 const foxImgLen = foxImg.length
-
+let index = ''
+imgLen.textContent = `/${foxImgLen}`
 for (let i = 0;i < foxImgLen;i++) {
     let li = document.createElement('li')
     let liImg = document.createElement('img')
@@ -18,21 +20,22 @@ for(let i = 0; i< foxImgLen; i++){
     img[i].addEventListener('click', () => {
         lightbox.style.display = 'block'
         lightboxImg.src = img[i].src
-        imgNUm.textContent = i + 1
+        imgNum.textContent = i + 1
+        index = i
     })
 }
 
 function changeImg(add) {
-    let index = (foxImg.indexOf(lightboxImg.attributes.src.value) + add + 6) % 6
-    imgNUm.textContent = index + 1
-    return index
+    index = (index + add + 6) % 6
+    imgNum.textContent = index + 1
+    lightboxImg.src = foxImg[index]
 }
 
 document.querySelector('.left').addEventListener('click', () => {
-    lightboxImg.src = `img/fox${changeImg(-1)}.jpg`
+    changeImg(-1)
 })
 document.querySelector('.right').addEventListener('click', () => {
-    lightboxImg.src = `img/fox${changeImg(1)}.jpg`
+    changeImg(1)
 })
 
 document.querySelector('.close').addEventListener('click',() => {
